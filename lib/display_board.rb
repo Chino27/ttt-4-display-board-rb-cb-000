4ff1ba43526d000abab55fd435a909bd52fc216b
Chino27 <alanccastillo@gmail.com>
2017-10-04 22:35:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 def display_board(board)
   board = [" "," "," "," "," "," "," "," "," "]
- 
-    |   |   
+
+    |   |
  -----------
-    |   |   
+    |   |
  -----------
-    |   |   
-  
+    |   |
+
 end# Define display_board that accepts a board and prints
 # out the current state.
